2fc400071dce4c49e57e3e4f57093cd4688cb57b
Alex Seif <alex.seif@gmail.com>
2015-09-25 18:18:34 +0200

    Customer first work

---
+++ README.md
@@ -0,0 +1,4 @@
+mevisa
+======
+
+A Symfony project created on September 11, 2015, 7:45 pm.
---
+++ src/MeVisa/CRMBundle/MeVisaCRMBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace MeVisa\CRMBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class MeVisaCRMBundle extends Bundle
+{
+}
