fbd7cf7b6dd04cac56a9882cf27bff20c7386b11
Alex Seif <alex.seif@gmail.com>
2015-10-05 04:53:41 +0200

    Currency management

---
+++ src/AppBundle/Controller/CurrencyController.php
@@ -0,0 +1,143 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use AppBundle\Entity\Currency;
+use AppBundle\Form\CurrencyType;
+
+/**
+ * Currency controller.
+ *
+ * @Route("/admin/currency")
+ */
+class CurrencyController extends Controller
+{
+
+    /**
+     * Lists all Currency entities.
+     *
+     * @Route("/", name="admin_currency")
+     * @Method("GET")
+     * @Template()
+     */
+    public function indexAction()
+    {
+        // Form creation
+        $entity = new Currency();
+        $form = $this->createCreateForm($entity);
+
+        //List details
+        $em = $this->getDoctrine()->getManager();
+
+        $entities = $em->getRepository('AppBundle:Currency')->findBy(array(), array('createdAt' => 'Desc'), 7);
+
+        return array(
+            'entity' => $entity,
+            'create_form' => $form->createView(),
+            'entities' => $entities,
+        );
+    }
+
+    /**
+     * Creates a new Currency entity.
+     *
+     * @Route("/", name="admin_currency_create")
+     * @Method("POST")
+     * 
+     */
+    public function createAction(Request $request)
+    {
+        $entity = new Currency();
+        $form = $this->createCreateForm($entity);
+        $form->handleRequest($request);
+        $entity->setCur1('USD');
+        $entity->setCur2('RUB');
+        $entity->setCreatedAt(new \DateTime());
+
+        if ($form->isValid()) {
+            var_dump($entity);
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($entity);
+            $em->flush();
+
+            $this->addFlash('success', 'Your changes were saved!');
+
+            return $this->redirect($this->generateUrl('admin_currency'));
+        }
+        return $this->redirect($this->generateUrl('admin_currency'));
+
+//        return array(
+//            'entity' => $entity,
+//            'form' => $form->createView(),
+//        );
+    }
+
+    /**
+     * Creates a form to create a Currency entity.
+     *
+     * @param Currency $entity The entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createCreateForm(Currency $entity)
+    {
+        $form = $this->createForm(new CurrencyType(), $entity, array(
+            'action' => $this->generateUrl('admin_currency_create'),
+            'method' => 'POST',
+        ));
+
+        $form->add('submit', 'submit', array('label' => 'Set'), array('attr' => array('class', 'pull-right')));
+
+        return $form;
+    }
+
+    /**
+     * Deletes a Currency entity.
+     *
+     * @Route("/{id}", name="admin_currency_delete")
+     * @Method("DELETE")
+     */
+    public function deleteAction(Request $request, $id)
+    {
+        $form = $this->createDeleteForm($id);
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $entity = $em->getRepository('AppBundle:Currency')->find($id);
+
+            if (!$entity) {
+                throw $this->createNotFoundException('Unable to find Currency entity.');
+            }
+
+            $em->remove($entity);
+            $em->flush();
+        }
+
+        return $this->redirect($this->generateUrl('admin_currency'));
+    }
+
+    /**
+     * Creates a form to delete a Currency entity by id.
+     *
+     * @param mixed $id The entity id
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm($id)
+    {
+        return $this->createFormBuilder()
+                        ->setAction($this->generateUrl('admin_currency_delete', array('id' => $id)))
+                        ->setMethod('DELETE')
+                        ->add('submit', 'submit', array('label' => 'Delete'))
+                        ->getForm()
+        ;
+    }
+
+}
---
+++ src/AppBundle/Entity/Currency.php
@@ -0,0 +1,155 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Currency
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class Currency
+{
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="cur1", type="string", length=4)
+     */
+    private $cur1;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="cur2", type="string", length=4)
+     */
+    private $cur2;
+
+    /**
+     * @var float
+     *
+     * @ORM\Column(name="value", type="float")
+     */
+    private $value;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="createdAt", type="datetime")
+     */
+    private $createdAt;
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set cur1
+     *
+     * @param string $cur1
+     * @return Currency
+     */
+    public function setCur1($cur1)
+    {
+        $this->cur1 = $cur1;
+
+        return $this;
+    }
+
+    /**
+     * Get cur1
+     *
+     * @return string 
+     */
+    public function getCur1()
+    {
+        return $this->cur1;
+    }
+
+    /**
+     * Set cur2
+     *
+     * @param string $cur2
+     * @return Currency
+     */
+    public function setCur2($cur2)
+    {
+        $this->cur2 = $cur2;
+
+        return $this;
+    }
+
+    /**
+     * Get cur2
+     *
+     * @return string 
+     */
+    public function getCur2()
+    {
+        return $this->cur2;
+    }
+
+    /**
+     * Set value
+     *
+     * @param float $value
+     * @return Currency
+     */
+    public function setValue($value)
+    {
+        $this->value = $value;
+
+        return $this;
+    }
+
+    /**
+     * Get value
+     *
+     * @return float 
+     */
+    public function getValue()
+    {
+        return $this->value;
+    }
+
+    /**
+     * Set createdAt
+     *
+     * @param \DateTime $createdAt
+     * @return Currency
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * Get createdAt
+     *
+     * @return \DateTime 
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+}
