b18852b60b0c01ff5c357a6bc9fc20a7289f6235
Alex Seif <alex.seif@gmail.com>
2015-10-15 05:20:17 +0200

    Todos note

---
+++ src/MeVisa/TODOS.php
@@ -0,0 +1,15 @@
+<?php
+
+/* 
+ *  The following content was designed & implemented under AlexSeif.com
+ */
+
+// TODO: Channel
+// TODO: Payment Method
+// TODO: Payment
+// TODO: Order
+// TODO: Order Products
+// TODO: Order Payment
+// TODO: Order Customer
+// TODO: Order details (Companions)
+// TODO: Menu fix
