e28ebcf7f6856e839cf0285704ac8cadb2523093
Alex Seif <alex.seif@gmail.com>
2015-10-23 20:41:54 +0200

    Incomplete fix of Product prices

--- src/MeVisa/ERPBundle/Form/OrderProductsType.php
+++ src/MeVisa/ERPBundle/Form/OrderProductsType.php
@@ -8,23 +8,21 @@ use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 use Symfony\Component\Form\FormEvent;
 use Symfony\Component\Form\FormEvents;
 
-class OrderProductsType extends AbstractType
-{
+class OrderProductsType extends AbstractType {
 
     /**
      * @param FormBuilderInterface $builder
      * @param array $options
      */
-    public function buildForm(FormBuilderInterface $builder, array $options)
-    {
+    public function buildForm(FormBuilderInterface $builder, array $options) {
         $builder
                 ->add('product', 'entity', array(
-                    'class' => 'MeVisaERPBundle:Products',
+                    'class' => 'MeVisa\ERPBundle\Entity\Products',
                     'choice_label' => 'name',
 //                    'placeholder' => 'Select Product',
-                    'multiple' => true,
-                    'expanded' => true,
-                    'label' => false,
+//                    'multiple' => true,
+//                    'expanded' => true,
+//                    'label' => false,
                     'attr' => array(
                         'class' => 'product_id',
                     )
@@ -51,8 +49,7 @@ class OrderProductsType extends AbstractType
     /**
      * @param OptionsResolverInterface $resolver
      */
-    public function setDefaultOptions(OptionsResolverInterface $resolver)
-    {
+    public function setDefaultOptions(OptionsResolverInterface $resolver) {
         $resolver->setDefaults(array(
             'data_class' => 'MeVisa\ERPBundle\Entity\OrderProducts'
         ));
@@ -61,8 +58,7 @@ class OrderProductsType extends AbstractType
     /**
      * @return string
      */
-    public function getName()
-    {
+    public function getName() {
         return 'mevisa_erpbundle_orderproducts';
     }
 
--- src/MeVisa/ERPBundle/Form/ProductPricesType.php
+++ src/MeVisa/ERPBundle/Form/ProductPricesType.php
@@ -6,49 +6,33 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
-class ProductPricesType extends AbstractType
-{
+class ProductPricesType extends AbstractType {
 
     /**
      * @param FormBuilderInterface $builder
      * @param array $options
      */
-    public function buildForm(FormBuilderInterface $builder, array $options)
-    {
+    public function buildForm(FormBuilderInterface $builder, array $options) {
         $builder
-                ->add('age', 'choice', array(
-                    'choices' => array('all' => 'All',
-                        'i' => 'Infant',
-                        'c' => 'Child',
-                        'a' => 'Adult')
+                ->add('cost', 'money', array(
+                    'currency' => 'RUB',
+                    'divisor' => 100
                 ))
-                ->add('cost')
-                ->add('costCurrency', 'choice', array(
-                    'choices' => array(
-                        'USD' => 'USD',
-                        'RUB' => 'RUB',),
-                    'attr' => array('class' => 'pull-right')
-                ))
-                ->add('price')
-                ->add('priceCurrency', 'choice', array(
-                    'choices' => array(
-                        'USD' => 'USD',
-                        'RUB' => 'RUB',)
+                ->add('price', 'money', array(
+                    'currency' => 'RUB',
+                    'divisor' => 100
                 ))
                 ->add('product', 'entity', array(
                     'class' => 'MeVisaERPBundle:Products',
                     'choice_label' => 'name',
                 ))
         ;
-//        'hidden', array(
-//    'data' => 'abcdef',
     }
 
     /**
      * @param OptionsResolverInterface $resolver
      */
-    public function setDefaultOptions(OptionsResolverInterface $resolver)
-    {
+    public function setDefaultOptions(OptionsResolverInterface $resolver) {
         $resolver->setDefaults(array(
             'data_class' => 'MeVisa\ERPBundle\Entity\ProductPrices'
         ));
@@ -57,8 +41,7 @@ class ProductPricesType extends AbstractType
     /**
      * @return string
      */
-    public function getName()
-    {
+    public function getName() {
         return 'mevisa_erpbundle_productprices';
     }
 
