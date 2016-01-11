64a1ffd34c94a1d99e979057ee3fe4cb3f857318
Alex Seif <alex.seif@gmail.com>
2016-01-11 04:49:09 +0200

    Service listener for mysql charset fix

---
+++ src/AppBundle/Service/OnConnect.php
@@ -0,0 +1,19 @@
+<?php
+
+//file src/AppBundle/EventListener/OnConnect.php
+
+namespace AppBundle\EventListener;
+
+use Doctrine\Common\Persistence\Event\LifecycleEventArgs;
+use Doctrine\ORM\EntityManager;
+
+class OnConnect
+{
+
+    public function postConnect($event)
+    {
+        $conn = $event->getConnection();
+        $conn->executeQuery("SET NAMES UTF8");
+    }
+
+}
