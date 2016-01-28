1340ce84521bacf19e1f09ea3a375b305fdbcc91
Alex Seif <alex.seif@gmail.com>
2016-01-29 00:50:42 +0200

    Dashboard ajax prep work

--- src/Admin/AdminBundle/Controller/DefaultController.php
+++ src/Admin/AdminBundle/Controller/DefaultController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class DefaultController extends Controller
 {
@@ -24,6 +25,36 @@ class DefaultController extends Controller
         $post = $em->getRepository('MeVisaERPBundle:Orders')->findAllByState("post");
         $completed = $em->getRepository('MeVisaERPBundle:Orders')->findAllByState("approved");
 
+        return array(
+            "pending" => $pending,
+            "backoffice" => $backoffice,
+            "document" => $document,
+            "post" => $post,
+            "completed" => $completed,
+        );
+    }
+
+    /**
+     * @Route("/ajax", name="dashboard_ajax")
+     * @Template()
+     */
+    public function dashboardAjaxAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $pending = $em->getRepository('MeVisaERPBundle:Orders')->findAllPending("pending");
+        $backoffice = $em->getRepository('MeVisaERPBundle:Orders')->findAllByState("backoffice");
+        $document = $em->getRepository('MeVisaERPBundle:Orders')->findAllByState("document");
+        $post = $em->getRepository('MeVisaERPBundle:Orders')->findAllByState("post");
+        $completed = $em->getRepository('MeVisaERPBundle:Orders')->findAllByState("approved");
+
+//        return new JsonResponse(array(
+//            "pending" => $pending,
+//            "backoffice" => $backoffice,
+//            "document" => $document,
+//            "post" => $post,
+//            "completed" => $completed,
+//        ));
         return array(
             "pending" => $pending,
             "backoffice" => $backoffice,
