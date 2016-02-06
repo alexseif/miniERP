e007b2aa752fafcc4db46324afa291978e18476b
Alex Seif <alex.seif@gmail.com>
2016-02-06 20:36:31 +0200

    testing invoices prod

--- src/MeVisa/ERPBundle/Controller/OrdersController.php
+++ src/MeVisa/ERPBundle/Controller/OrdersController.php
@@ -551,29 +551,30 @@ class OrdersController extends Controller
 
         $myProjectDirectory = __DIR__ . '/../../../../';
         $invoicePath = $myProjectDirectory . 'web/invoices/mevisa-invoice-' . $order->getNumber() . '-' . $invoice->getId() . '.pdf';
-        $fs = new Filesystem();
-
-        if (!$fs->exists($invoicePath)) {
-
-//            $snappy = new Pdf($myProjectDirectory . 'vendor/h4cc/wkhtmltopdf-i386/bin/wkhtmltopdf-i386');
-            $snappy = $this->get('knp_snappy.pdf');
-
-            $snappy->setOption('title', 'MeVisa Invoice ' . $order->getNumber() . '-' . $invoice->getId());
-            $snappy->setOption('encoding', 'UTF-8');
-            $snappy->setOption('lowquality', null);
-
-//            $renderedView = $this->renderView(
-//                    'MeVisaERPBundle:Orders:invoicepdf.html.twig', array(
-//                'order' => $order,
-//                'invoice' => $invoice
-//                    )
-//            );
-//            $snappy->generateFromHtml($renderedView, $invoicePath);
-            $snappy->generate('http://google.com', $invoicePath);
-            $invoice->setCreatedAt(new \DateTime());
-
-            $em->flush();
-        }
+        $container->get('knp_snappy.pdf')->generate('http://www.google.fr', '/path/to/the/file.pdf');
+//        $fs = new Filesystem();
+//
+//        if (!$fs->exists($invoicePath)) {
+//
+////            $snappy = new Pdf($myProjectDirectory . 'vendor/h4cc/wkhtmltopdf-i386/bin/wkhtmltopdf-i386');
+//            $snappy = $this->get('knp_snappy.pdf');
+//
+//            $snappy->setOption('title', 'MeVisa Invoice ' . $order->getNumber() . '-' . $invoice->getId());
+//            $snappy->setOption('encoding', 'UTF-8');
+//            $snappy->setOption('lowquality', null);
+//
+////            $renderedView = $this->renderView(
+////                    'MeVisaERPBundle:Orders:invoicepdf.html.twig', array(
+////                'order' => $order,
+////                'invoice' => $invoice
+////                    )
+////            );
+////            $snappy->generateFromHtml($renderedView, $invoicePath);
+//            $snappy->generate('http://google.com', $invoicePath);
+//            $invoice->setCreatedAt(new \DateTime());
+//
+//            $em->flush();
+//        }
         return true;
     }
 
