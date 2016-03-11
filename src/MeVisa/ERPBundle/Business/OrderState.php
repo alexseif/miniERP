02b567a2fe78db9ad70d5889d4691691d37027e9
Alex Seif <alex.seif@gmail.com>
2016-03-11 10:49:51 +0200

    Removing refunded state

--- src/MeVisa/ERPBundle/Business/OrderState.php
+++ src/MeVisa/ERPBundle/Business/OrderState.php
@@ -24,7 +24,7 @@ class OrderState
         $this->addState('post', 'Post', 'primary');
         $this->addState('approved', 'Approved', 'success');
         $this->addState('rejected', 'Rejected', 'danger');
-        $this->addState('refunded', 'Refunded', 'danger');
+//        $this->addState('refunded', 'Refunded', 'danger');
         $this->addState('cancelled', 'Cancelled', 'danger');
 
         $this->addChild('backoffice', 'document');
@@ -33,8 +33,8 @@ class OrderState
         $this->addChild('post', 'backoffice');
         $this->addChild('backoffice', 'cancelled');
         $this->addChild('cancelled', 'backoffice');
-        $this->addChild('backoffice', 'refunded');
-        $this->addChild('refunded', 'backoffice');
+//        $this->addChild('backoffice', 'refunded');
+//        $this->addChild('refunded', 'backoffice');
 
         $this->addChild('document', 'post');
         $this->addChild('post', 'document');
