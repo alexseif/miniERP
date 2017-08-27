168fbdf58436d03d4134c59c07d4a0bb2d328b70
Alex Seif <alex.seif@gmail.com>
2017-08-27 16:40:01 +0200

    Removing deleted orders from reports

--- src/MeVisa/ERPBundle/Entity/OrdersRepository.php
+++ src/MeVisa/ERPBundle/Entity/OrdersRepository.php
@@ -211,13 +211,15 @@ class OrdersRepository extends EntityRepository
             ->leftJoin('o.customer', 'c')
             ->leftJoin('o.orderPayments', 'opa')
             ->leftJoin('o.orderProducts', 'opr')
-            ->where('opa.state = ?3 ')
+            ->where('o.state != ?4 ')
+            ->andWhere('opa.state = ?3 ')
             ->andWhere('MONTHNAME(o.createdAt) = ?1')
             ->andWhere('YEAR(o.createdAt) = ?2')
             ->orderBy('o.createdAt, o.wcId')
             ->setParameter('1', $month)
             ->setParameter('2', $year)
             ->setParameter('3', "PAID")
+            ->setParameter('4', "deleted")
             ->getQuery()
             ->getResult();
   }
@@ -229,13 +231,15 @@ class OrdersRepository extends EntityRepository
             ->leftJoin('o.customer', 'c')
             ->leftJoin('o.orderPayments', 'opa')
             ->leftJoin('o.orderProducts', 'opr')
-            ->where('opa.state = ?3 ')
+            ->where('o.state != ?4 ')
+            ->andWhere('opa.state = ?3 ')
             ->andWhere('o.createdAt >= ?1')
             ->andWhere('o.createdAt <= ?2')
             ->orderBy('o.createdAt, o.wcId')
             ->setParameter('1', $from)
             ->setParameter('2', $to)
             ->setParameter('3', "PAID")
+            ->setParameter('4', "deleted")
             ->getQuery()
             ->getResult();
   }
@@ -247,7 +251,8 @@ class OrdersRepository extends EntityRepository
             ->leftJoin('o.customer', 'c')
             ->leftJoin('o.orderPayments', 'opa')
             ->leftJoin('o.orderProducts', 'opr')
-            ->where('opa.state = ?3 ')
+            ->where('o.state != ?4 ')
+            ->andWhere('opa.state = ?3 ')
             ->andWhere('MONTHNAME(o.createdAt) = ?1')
             ->andWhere('YEAR(o.createdAt) = ?2')
             ->andWhere("opa.method != 'cash'")
@@ -255,6 +260,7 @@ class OrdersRepository extends EntityRepository
             ->setParameter('1', $month)
             ->setParameter('2', $year)
             ->setParameter('3', "PAID")
+            ->setParameter('4', "deleted")
             ->getQuery()
             ->getResult();
   }
@@ -266,7 +272,8 @@ class OrdersRepository extends EntityRepository
             ->leftJoin('o.customer', 'c')
             ->leftJoin('o.orderPayments', 'opa')
             ->leftJoin('o.orderProducts', 'opr')
-            ->where('opa.state = ?3 ')
+            ->where('o.state = ?4 ')
+            ->andWhere('opa.state = ?3 ')
             ->andWhere('o.createdAt >= ?1')
             ->andWhere('o.createdAt <= ?2')
             ->andWhere("opa.method != 'cash'")
@@ -274,6 +281,7 @@ class OrdersRepository extends EntityRepository
             ->setParameter('1', $from)
             ->setParameter('2', $to)
             ->setParameter('3', "PAID")
+            ->setParameter('4', "deleted")
             ->getQuery()
             ->getResult();
   }
@@ -284,6 +292,8 @@ class OrdersRepository extends EntityRepository
             ->select('v.id as vendor_id, v.name as vendor_name, YEAR(o.createdAt) as gBYear, MONTHNAME(o.createdAt) as gBMonth, MONTH(o.createdAt) as gMonth ')
             ->leftJoin('o.orderProducts', 'opr')
             ->leftJoin('opr.vendor', 'v')
+            ->where('o.state != ?4 ')
+            ->setParameter('4', "deleted")
             ->orderBy("o.createdAt, o.wcId")
             ->groupBy('opr.vendor')
             ->addGroupBy('gBYear')
@@ -299,7 +309,8 @@ class OrdersRepository extends EntityRepository
             ->leftJoin('o.customer', 'c')
             ->leftJoin('o.orderPayments', 'opa')
             ->leftJoin('o.orderProducts', 'opr')
-            ->where('opa.state = ?3 ')
+            ->where('o.state != ?5 ')
+            ->andWhere('opa.state = ?3 ')
             ->andWhere('MONTHNAME(o.createdAt) = ?1')
             ->andWhere('YEAR(o.createdAt) = ?2')
             ->andWhere('opr.vendor = ?4')
@@ -308,6 +319,7 @@ class OrdersRepository extends EntityRepository
             ->setParameter('2', $year)
             ->setParameter('3', "PAID")
             ->setParameter('4', $vendor_id)
+            ->setParameter('5', "deleted")
             ->getQuery()
             ->getResult();
   }
