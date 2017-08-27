8d70009c3292291197e930c8b4721387635164e9
Alex Seif <alex.seif@gmail.com>
2017-08-27 14:13:08 +0200

    Revert "Installting doctrine migrations"
    This reverts commit cf0836b6a3046133578f58ed1ab27da7300a7ba2.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -6,42 +6,41 @@ use Symfony\Component\Config\Loader\LoaderInterface;
 class AppKernel extends Kernel
 {
 
-  public function registerBundles()
-  {
-    $bundles = array(
-      new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
-      new Symfony\Bundle\SecurityBundle\SecurityBundle(),
-      new Symfony\Bundle\TwigBundle\TwigBundle(),
-      new Symfony\Bundle\MonologBundle\MonologBundle(),
-      new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
-      new Symfony\Bundle\AsseticBundle\AsseticBundle(),
-      new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
-      new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-      new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
-      new AppBundle\AppBundle(),
-      new MeVisa\CRMBundle\MeVisaCRMBundle(),
-      new MeVisa\ERPBundle\MeVisaERPBundle(),
-      new Admin\AdminBundle\AdminAdminBundle(),
-      new Admin\SecurityBundle\AdminSecurityBundle(),
-      new FOS\UserBundle\FOSUserBundle(),
-      new Sonata\IntlBundle\SonataIntlBundle(),
-      new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
-      new Knp\Bundle\TimeBundle\KnpTimeBundle(),
-    );
+    public function registerBundles()
+    {
+        $bundles = array(
+            new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
+            new Symfony\Bundle\SecurityBundle\SecurityBundle(),
+            new Symfony\Bundle\TwigBundle\TwigBundle(),
+            new Symfony\Bundle\MonologBundle\MonologBundle(),
+            new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
+            new Symfony\Bundle\AsseticBundle\AsseticBundle(),
+            new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
+            new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new AppBundle\AppBundle(),
+            new MeVisa\CRMBundle\MeVisaCRMBundle(),
+            new MeVisa\ERPBundle\MeVisaERPBundle(),
+            new Admin\AdminBundle\AdminAdminBundle(),
+            new Admin\SecurityBundle\AdminSecurityBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
+            new Sonata\IntlBundle\SonataIntlBundle(),
+            new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
+            new Knp\Bundle\TimeBundle\KnpTimeBundle(),
+        );
 
-    if (in_array($this->getEnvironment(), array('dev', 'test'))) {
-      $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
-      $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
-      $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
-      $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+        if (in_array($this->getEnvironment(), array('dev', 'test'))) {
+            $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
+            $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
+            $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
+            $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+        }
+
+        return $bundles;
     }
 
-    return $bundles;
-  }
-
-  public function registerContainerConfiguration(LoaderInterface $loader)
-  {
-    $loader->load($this->getRootDir() . '/config/config_' . $this->getEnvironment() . '.yml');
-  }
+    public function registerContainerConfiguration(LoaderInterface $loader)
+    {
+        $loader->load($this->getRootDir() . '/config/config_' . $this->getEnvironment() . '.yml');
+    }
 
 }
