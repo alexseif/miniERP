1284a46398a5bdb330290f49bd493652c6cdae38
Alex Seif <alex.seif@gmail.com>
2017-09-01 21:39:52 +0200

    Disabling uneeded services

--- src/MeVisa/ERPBundle/DependencyInjection/MeVisaERPExtension.php
+++ src/MeVisa/ERPBundle/DependencyInjection/MeVisaERPExtension.php
@@ -24,7 +24,7 @@ class MeVisaERPExtension extends Extension
     $config = $this->processConfiguration($configuration, $configs);
 
     $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
-    $loader->load('services.xml');
+    // $loader->load('services.xml');
   }
 
 }
