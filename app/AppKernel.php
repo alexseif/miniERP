6e9d3b8480edc6f1c2fe7d50d22fef3282963d95
Alex Seif <alex.seif@gmail.com>
2017-09-29 23:42:40 +0200

    Integrating with Open Currency Exchange and displaying in the theme head

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -6,41 +6,42 @@ use Symfony\Component\Config\Loader\LoaderInterface;
 class AppKernel extends Kernel
 {
 
-    public function registerBundles()
-    {
-        $bundles = array(
-            new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
-            new Symfony\Bundle\SecurityBundle\SecurityBundle(),
-            new Symfony\Bundle\TwigBundle\TwigBundle(),
-            new Symfony\Bundle\MonologBundle\MonologBundle(),
-            new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
-            new Symfony\Bundle\AsseticBundle\AsseticBundle(),
-            new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
-            new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-	    new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
-            new AppBundle\AppBundle(),
-            new MeVisa\CRMBundle\MeVisaCRMBundle(),
-            new MeVisa\ERPBundle\MeVisaERPBundle(),
+  public function registerBundles()
+  {
+    $bundles = array(
+      new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
+      new Symfony\Bundle\SecurityBundle\SecurityBundle(),
+      new Symfony\Bundle\TwigBundle\TwigBundle(),
+      new Symfony\Bundle\MonologBundle\MonologBundle(),
+      new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
+      new Symfony\Bundle\AsseticBundle\AsseticBundle(),
+      new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
+      new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+      new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
+      new AppBundle\AppBundle(),
+      new MeVisa\CRMBundle\MeVisaCRMBundle(),
+      new MeVisa\ERPBundle\MeVisaERPBundle(),
 //            new Admin\SecurityBundle\AdminSecurityBundle(),
-            new FOS\UserBundle\FOSUserBundle(),
-            new Sonata\IntlBundle\SonataIntlBundle(),
-            new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
-            new Knp\Bundle\TimeBundle\KnpTimeBundle(),
-        );
+      new FOS\UserBundle\FOSUserBundle(),
+      new Sonata\IntlBundle\SonataIntlBundle(),
+      new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
+      new Knp\Bundle\TimeBundle\KnpTimeBundle(),
+      new Mrzard\OpenExchangeRatesBundle\OpenExchangeRatesBundle(),
+    );
 
-        if (in_array($this->getEnvironment(), array('dev', 'test'))) {
-            $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
-            $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
-            $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
-            $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
-        }
-
-        return $bundles;
+    if (in_array($this->getEnvironment(), array('dev', 'test'))) {
+      $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
+      $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
+      $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
+      $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
     }
 
-    public function registerContainerConfiguration(LoaderInterface $loader)
-    {
-        $loader->load($this->getRootDir() . '/config/config_' . $this->getEnvironment() . '.yml');
-    }
+    return $bundles;
+  }
+
+  public function registerContainerConfiguration(LoaderInterface $loader)
+  {
+    $loader->load($this->getRootDir() . '/config/config_' . $this->getEnvironment() . '.yml');
+  }
 
 }
